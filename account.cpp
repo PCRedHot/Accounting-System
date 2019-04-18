d52a28243c39f84f088caf72197a6d9a2804a108
PCRedHot <parrychoi1109@gmail.com>
2019-04-18 21:21:59 +0800

    account update

--- account.cpp
+++ account.cpp
@@ -1,4 +1,55 @@
 #include <iostream>
+#include <string>
 #include "account.h"
 
 using namespace std;
+
+void account::setName(string InputName){
+  name = InputName;
+};
+
+void account::setBalance(float val){
+  balance = val;
+};
+
+float account::addAmount(float val){
+  balance += val;
+  return balance;
+};
+
+void account::setPrevious(account acc){
+  previous = &acc;
+};
+
+void account::setPrevious(account* acc){
+  previous = acc;
+};
+
+void account::setNext(account acc){
+  next = &acc;
+};
+
+void account::setNext(account* acc){
+  next = acc;
+};
+
+string account::getName(){
+  return name;
+};
+
+float account::getBalance(){
+  return balance;
+};
+
+account* account::getNext(){
+  return next;
+};
+
+account* account::getPrevious(){
+  return previous;
+};
+
+string account::getData(){
+  string data = date + '\t' + to_string(balance);
+  return data;
+};
--- account.h
+++ account.h
@@ -6,9 +6,16 @@ public:
   account* next;
   void setName(string);
   void setBalance(float);
-  void addAmount(float);
+  float addAmount(float);
   void setPrevious(account);
   void setPrevious(account*);
   void setNext(account);
   void setNext(account*);
+
+  string getName();
+  float getBalance();
+  account* getNext();
+  account* getPrevious();
+
+  string getData();
 }
