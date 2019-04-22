753c70ab4997984cd0f6b8e2248ede16750a9f82
XsChen524 <43726972+XsChen524@users.noreply.github.com>
2019-04-22 13:12:15 +0800

    Update Transaction.h

--- Transaction.h
+++ Transaction.h
@@ -1,13 +1,14 @@
-#include<string>
+#include <string>
+#include <iostream>
 #include"account.h"
 
 class Transaction {
 public:
- 	int id;		       //id of the transaction
-	string date;	   //date of the transaction
-	string name_from; //the first account of the transaction
-	string name_to; //the second account of the transaction
-	float amount;	   //the transaction amount
+  int id;		       //id of the transaction
+  string date;	   //date of the transaction
+  string name_from; //the first account of the transaction
+  string name_to; //the second account of the transaction
+  float amount;	   //the transaction amount
   Transaction* previous;
   Transaction* next;
 
