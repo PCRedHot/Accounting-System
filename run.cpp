3f0f36860fd847a542ccea49795fd2aa3e447f29
XsChen524 <43726972+XsChen524@users.noreply.github.com>
2019-04-22 13:16:03 +0800

    Update run.cpp

--- run.cpp
+++ run.cpp
@@ -14,6 +14,12 @@ static account* accHead; //head account pointer
 static transaction* tranHead;  //head transaction pointer
 
 int main(){
+  //Create head and tail of transaction, point to nullptr
+  Transaction* head_T = nullptr;
+  Transaction* tail_T = nullptr;
+  int order_T = 1;
+  //
+  
   //get accounts from file
   ifstream file;
   file.open("account");
@@ -49,7 +55,20 @@ int main(){
       cin >> userInput;
       //**TO-DO**//
       //reminders: check if it is the first account to be created!! if so, accHead = newAcc
-
+        if (userInput == "Transction"){
+          Transaction* current_T = new Transaction (order);
+          if( head_T = nullptr ){
+            head_T = current_T;
+            tail_T = current_T;
+            current_T = head->next;
+            order++;
+          }
+          else{
+            tail_T->next = current_T;
+            tail_T = current_T->next;
+            order++;
+          }
+        }
   }
 
   //store accounts and transactions to files
